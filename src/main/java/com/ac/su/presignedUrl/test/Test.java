08dcce622623d97462c444fc70cba78ad5f06e3b
이정훈 <niko36@naver.com>
2024-08-12 15:19:18 +0900

    Add execute permission to gradlew

--- src/main/java/com/ac/su/presignedUrl/test/Test.java
+++ src/main/java/com/ac/su/presignedUrl/test/Test.java
@@ -6,4 +6,7 @@ public class Test {
     //application.properties 파일에 AWS 시크릿키 등 민감정보가 올라가있고
     //.gitingore 파일에 application.properties을 추가한 상태에서
     // github에 해당 내용이 푸시가 되는지 확인
+
+    //테스트 결과:
+
 }
